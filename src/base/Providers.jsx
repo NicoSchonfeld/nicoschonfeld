ae6ac594146018d8879b7ae544946c7f119de8f1
Nico Schönfeld <75105918+NicoSchonfeld@users.noreply.github.com>
2023-09-22 13:15:15 -0300

    Save Project

--- src/base/Providers.jsx
+++ src/base/Providers.jsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 import NavbarComp from "@/components/Navbar";
 
 export function Providers({ children }) {
+  const { theme, setTheme } = useTheme();
+
   return (
     <NextUIProvider>
-      <NextThemesProvider /* attribute="class" */ defaultTheme="dark">
+      <NextThemesProvider /* attribute="class" */ defaultTheme={theme}>
         <NavbarComp />
         {children}
       </NextThemesProvider>
