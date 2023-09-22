0fc94d9cf8d1367ce91daa72a69bfd6d76328190
Nico Schönfeld <75105918+NicoSchonfeld@users.noreply.github.com>
2023-09-21 23:24:46 -0300

    Save Project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import { Button, Link } from "@nextui-org/react";
+import { Button, Link, Snippet, Tooltip } from "@nextui-org/react";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { FaInstagram } from "react-icons/fa";
 import { RiTwitterXFill } from "react-icons/ri";
 import { SiVercel } from "react-icons/si";
+import { motion, AnimatePresence } from "framer-motion";
 
 import moment from "moment/moment";
+import { CopyIcon } from "./CopyIcon";
+import { CheckIcon } from "./CheckIcon";
 moment.locale("es");
 
 const formato = "YYYY";
@@ -16,63 +19,125 @@ const AñoActual = moment();
 
 const Footer = () => {
   const { theme, setTheme } = useTheme();
+
+  const [copy, setCopy] = useState(false);
+
+  const toggleCopy = () => {
+    setCopy(true);
+    typeof window !== "undefined" &&
+      window.navigator.clipboard.writeText("nicoschonfeld88@gmail.com");
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCopy(false);
+    }, 5500);
+  }, [copy]);
+
   return (
-    <footer className="w-full py-10 h-auto flex flex-col items-center justify-center gap-5">
-      <div className="space-x-5">
-        <Link
-          href="https://www.linkedin.com/in/nicoschonfeld/"
+    <>
+      <footer className="w-full py-10 h-auto flex flex-col items-center justify-center gap-5">
+        <div className="space-x-5">
+          <Link
+            href="https://www.linkedin.com/in/nicoschonfeld/"
+            target="_blank"
+            className="text-2xl hover:scale-90 transition-all"
+            color="success"
+          >
+            <AiFillLinkedin />
+          </Link>
+
+          <Link
+            href="https://www.instagram.com/nicoschonfeld_/"
+            target="_blank"
+            className="text-2xl hover:scale-90 transition-all"
+            color="success"
+          >
+            <FaInstagram />
+          </Link>
+
+          <Link
+            href="https://github.com/NicoSchonfeld"
+            target="_blank"
+            className="text-2xl hover:scale-90 transition-all"
+            color="success"
+          >
+            <AiFillGithub />
+          </Link>
+
+          <Link
+            href="https://twitter.com/NicoSchonfeld__"
+            target="_blank"
+            className="text-2xl hover:scale-90 transition-all"
+            color="success"
+          >
+            <RiTwitterXFill />
+          </Link>
+        </div>
+
+        <Tooltip content="Copiar en el portapapeles">
+          <Button
+            className="flex items-center gap-3 bg-transparent text-base"
+            onClick={toggleCopy}
+          >
+            nicoschonfeld88@gmail.com {copy ? <CheckIcon /> : <CopyIcon />}
+          </Button>
+        </Tooltip>
+
+        <Button
+          variant="ghost"
+          as={Link}
+          href="https://vercel.com/"
           target="_blank"
-          className="text-2xl hover:scale-90 transition-all"
-          color="success"
         >
-          <AiFillLinkedin />
-        </Link>
+          Deployed on{" "}
+          <span className="flex items-center">
+            <SiVercel />
+            Vercel.
+          </span>
+        </Button>
 
-        <Link
-          href="https://www.instagram.com/nicoschonfeld_/"
-          target="_blank"
-          className="text-2xl hover:scale-90 transition-all"
-          color="success"
-        >
-          <FaInstagram />
-        </Link>
+        <p className="text-gray-500">
+          © {AñoActual.format(formato)} Nico Schönfeld.
+        </p>
+      </footer>
 
-        <Link
-          href="https://github.com/NicoSchonfeld"
-          target="_blank"
-          className="text-2xl hover:scale-90 transition-all"
-          color="success"
-        >
-          <AiFillGithub />
-        </Link>
+      <AnimatePresence>
+        {copy && (
+          <div className="fixed right-5 bottom-10 overflow-hidden py-2">
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 1, y: 60 }}
+              role="alert"
+              className="bg-[#9455D3] flex items-center gap-3 p-3 w-full rounded-xl"
+            >
+              <span>
+                <svg
+                  className="text-white"
+                  data-testid="geist-icon"
+                  fill="none"
+                  height="24"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path d="M20 6L9 17l-5-5" />
+                </svg>
+              </span>
 
-        <Link
-          href="https://twitter.com/NicoSchonfeld__"
-          target="_blank"
-          className="text-2xl hover:scale-90 transition-all"
-          color="success"
-        >
-          <RiTwitterXFill />
-        </Link>
-      </div>
-
-      <Button
-        variant="ghost"
-        as={Link}
-        href="https://vercel.com/"
-        target="_blank"
-      >
-        Deployed on{" "}
-        <span className="flex items-center">
-          <SiVercel />
-          Vercel.
-        </span>
-      </Button>
-
-      <p className="text-gray-500">
-        © {AñoActual.format(formato)} Nico Schönfeld.
-      </p>
-    </footer>
+              <p className="text-white sm:text-base text-sm">
+                ¡Se copió en el portapapeles!
+              </p>
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
